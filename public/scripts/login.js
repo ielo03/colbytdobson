0303671a0bf5ba019bb349bd8f8e26486fac0beb
ielo03 <89800883+ielo03@users.noreply.github.com>
2025-01-14 03:38:57 -0800

    login maybe working

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -55,7 +55,7 @@ async function refreshAccessToken() {
 }
 
 // Function to check and ensure a valid access token
-async function ensureAccessToken() {
+async function refreshAccessToken() {
     if (!App.accessToken) {
         console.log("No access token found. Attempting to refresh...");
 
@@ -168,7 +168,7 @@ window.addEventListener("load", async () => {
 
     try {
         if (App?.cookies?.refreshTokenExpiry || accessToken) {
-            const hasToken = await ensureAccessToken();
+            const hasToken = await refreshAccessToken();
             if (hasToken) {
                 return showLogout();
             }
