2cd405bc6e761a162af9cca197b2d6f57e49124b
ielo03 <89800883+ielo03@users.noreply.github.com>
2025-01-14 12:11:16 -0800

    remove frontend logs

--- public/scripts/loginRequired.js
+++ public/scripts/loginRequired.js
@@ -1,6 +1,5 @@
 window.addEventListener(`load`, () => {
-    const refreshTokenExpiry = window.MyApp?.cookies?.refreshTokenExpiry;
-    console.log(`refreshTokenExpiry: ${refreshTokenExpiry}`);
+    const refreshTokenExpiry = window.App?.cookies?.refreshTokenExpiry;
     if (refreshTokenExpiry !== undefined && refreshTokenExpiry > new Date()) {
         document.getElementById('main').style.display = 'block';
         document.getElementById('placeholder').style.display = 'none';
